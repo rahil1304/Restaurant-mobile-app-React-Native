c3363fac0fa032eb34560b05806b64616d0916d1
Rahil Sarvaiya <rahil1304@gmail.com>
2020-08-08 19:02:58 +0530

    setting up Redux

---
+++ redux/ActionCreators.js
@@ -0,0 +1,154 @@
+import * as ActionTypes from "./ActionTypes";
+import { baseUrl } from "../shared/baseUrl";
+
+export const fetchComments = () => (dispatch) => {
+  return fetch(baseUrl + "comments")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        var errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((comments) => dispatch(addComments(comments)))
+    .catch((error) => dispatch(commentsFailled(error.message)));
+};
+
+export const commentsFailled = (errMess) => ({
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: errMess,
+});
+
+export const addComments = (comments) => ({
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments,
+});
+
+export const fetchDishes = () => (dispatch) => {
+  dispatch(dishesLoading());
+
+  return fetch(baseUrl + "dishes")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        var errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((dishes) => dispatch(addDishes(dishes)))
+    .catch((error) => dispatch(dishesFailed(error.message)));
+};
+
+export const dishesLoading = () => ({
+  type: ActionTypes.DISHES_LOADING,
+});
+
+export const dishesFailed = (errMess) => ({
+  type: ActionTypes.DISHES_FAILED,
+  payload: errMess,
+});
+
+export const addDishes = (dishes) => ({
+  type: ActionTypes.ADD_DISHES,
+  payload: dishes,
+});
+
+export const fetchPromos = () => (dispatch) => {
+  dispatch(promosLoading());
+  return fetch(baseUrl + "promotions")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        var errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((promotions) => dispatch(addPromos(promotions)))
+    .catch((error) => dispatch(promosFailled(error.message)));
+};
+
+export const promosLoading = () => ({
+  type: ActionTypes.PROMOS_LOADING,
+});
+
+export const promosFailled = (errMess) => ({
+  type: ActionTypes.PROMOS_FAILED,
+  payload: errMess,
+});
+
+export const addPromos = (promotions) => ({
+  type: ActionTypes.ADD_PROMOS,
+  payload: promotions,
+});
+
+export const fetchLeaders = () => (dispatch) => {
+  dispatch(leadersLoading());
+  return fetch(baseUrl + "leaders")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        var errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then((response) => response.json())
+    .then((leaders) => dispatch(addLeaders(leaders)))
+    .catch((error) => dispatch(leadersFailled(error.message)));
+};
+
+export const leadersLoading = () => ({
+  type: ActionTypes.LEADERS_LOADING,
+});
+
+export const leadersFailled = (errMess) => ({
+  type: ActionTypes.LEADERS_FAILED,
+  payload: errMess,
+});
+
+export const addLeaders = (leaders) => ({
+  type: ActionTypes.ADD_LEADERS,
+  payload: leaders,
+});
