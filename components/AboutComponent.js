1d0948cd713d55a046a2c45c49b84cb32e1977c1
Rahil Sarvaiya <rahil1304@gmail.com>
2020-08-09 13:24:49 +0530

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return {
@@ -46,18 +47,36 @@ class About extends Component {
         />
       );
     };
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
+    if (this.props.leaders.isLoading) {
+      return (
+        <ScrollView>
+          <History />
+          <Card title='Corporate Leadership'>
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    } else if (this.props.leaders.errMess) {
+      <ScrollView>
         <History />
         <Card title='Corporate Leadership'>
-          <FlatList
-            data={this.props.leaders.leaders}
-            renderItem={renderAbout}
-            keyExtractor={(item) => item.id.toString()}
-          />
+          <Text>{this.props.leaders.errMess}</Text>
         </Card>
-      </SafeAreaView>
-    );
+      </ScrollView>;
+    } else {
+      return (
+        <SafeAreaView style={{ flex: 1 }}>
+          <History />
+          <Card title='Corporate Leadership'>
+            <FlatList
+              data={this.props.leaders.leaders}
+              renderItem={renderAbout}
+              keyExtractor={(item) => item.id.toString()}
+            />
+          </Card>
+        </SafeAreaView>
+      );
+    }
   }
 }
 
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, View, Text } from "react-native";
 import { Card } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return {
@@ -14,19 +15,29 @@ const mapStateToProps = (state) => {
 
 function RenderItem(props) {
   const item = props.item;
-  if (item != null) {
+  if (props.isLoading) {
+    return <Loading />;
+  } else if (props.errMess) {
     return (
-      <Card
-        featuredTitle={item.name}
-        featuredSubtitle={item.designation}
-        image={{ uri: baseUrl + item.image }}
-      >
-        <Text style={{ margin: 10 }}>{item.description}</Text>
-      </Card>
+      <View>
+        <Text>{props.errMess}</Text>
+      </View>
     );
   } else {
-    console.log("NOTHING");
-    return <Text>Hello</Text>;
+    if (item != null) {
+      return (
+        <Card
+          featuredTitle={item.name}
+          featuredSubtitle={item.designation}
+          image={{ uri: baseUrl + item.image }}
+        >
+          <Text style={{ margin: 10 }}>{item.description}</Text>
+        </Card>
+      );
+    } else {
+      console.log("NOTHING");
+      return <Text>Hello</Text>;
+    }
   }
 }
 
@@ -40,6 +51,8 @@ class Home extends Component {
       <ScrollView>
         <RenderItem
           item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
         />
         <RenderItem
           item={
@@ -47,11 +60,15 @@ class Home extends Component {
               (promo) => promo.featured
             )[0]
           }
+          isLoading={this.props.promotions.isLoading}
+          errMess={this.props.promotions.errMess}
         />
         <RenderItem
           item={
             this.props.leaders.leaders.filter((leader) => leader.featured)[0]
           }
+          isLoading={this.props.leaders.isLoading}
+          errMess={this.props.leaders.errMess}
         />
       </ScrollView>
     );
