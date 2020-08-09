27687e9e6664c450c41eac81558ab78be86e6e22
Rahil Sarvaiya <rahil1304@gmail.com>
2020-08-09 16:30:21 +0530

    Modal

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -19,21 +19,30 @@ class Reservation extends Component {
       guests: 1,
       smoking: false,
       date: "",
+      showModal: false,
     };
   }
   static navigationOption = {
     title: "Reserve Table",
   };
 
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
   handleReservation() {
     console.log(JSON.stringify(this.state));
+    this.toggleModal();
+    //this.resetForm();
+  }
+
+  resetForm() {
     this.setState({
       guests: 1,
       smoking: false,
       date: "",
     });
   }
-
   render() {
     return (
       <ScrollView>
@@ -98,6 +107,40 @@ class Reservation extends Component {
             accessibilityLabel='Learn more about this purple button'
           />
         </View>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.showModal}
+          onDismiss={() => {
+            this.toggleModal();
+            this.resetForm();
+          }}
+          onRequestClose={() => {
+            this.toggleModal();
+            this.resetForm();
+          }}
+        >
+          <View style={styles.modal}>
+            <Text style={styles.modalTitle}>Your Reservation</Text>
+            <Text style={styles.modalText}>
+              Number of Guests: {this.state.guests}
+            </Text>
+            <Text style={styles.modalText}>
+              Smoking? : {this.state.smoking ? "Yes" : "No"}
+            </Text>
+            <Text style={styles.modalText}>
+              Date and Time: {this.state.date}
+            </Text>
+            <Button
+              onPress={() => {
+                this.toggleModal();
+                this.resetForm();
+              }}
+              color='#512DA8'
+              title='Close'
+            />
+          </View>
+        </Modal>
       </ScrollView>
     );
   }
@@ -117,6 +160,22 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1,
+  },
+  modal: {
+    justifyContent: "center",
+    margin: 20,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    backgroundColor: "#512DA8",
+    textAlign: "center",
+    color: "white",
+    marginBottom: 20,
+  },
+  modalText: {
+    fontSize: 18,
+    margin: 10,
   },
 });
 
